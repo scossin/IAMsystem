9f4cb22b659c00d95a8a87765c41d13e6c3e6427
cossins <sebastien.cossin@chu-bordeaux.fr>
2020-12-02 22:20:40 +0100

    implement toString method in DetectOutput class

--- IAMsystem/src/main/java/fr/erias/IAMsystem/detect/DetectOutput.java
+++ IAMsystem/src/main/java/fr/erias/IAMsystem/detect/DetectOutput.java
@@ -61,4 +61,32 @@ public class DetectOutput {
 		output.put("ct", jsonArray);
 		return(output);
 	}
+	
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(this.getCTcodes().size() + " terms detected.");
+		sb.append("\n");
+		int count = 1;
+		for (CTcode ct : this.getCTcodes()) {
+			sb.append("term number " + count + ":");
+			sb.append("\n");
+			sb.append("\t label in terminology: '" + ct.getLabel() + "'");
+			sb.append("\n");
+			sb.append("\t written exactly like this in the sentence: '" + ct.getCandidateTermString() + "'");
+			sb.append("\n");
+			sb.append("\t code in terminology: " + ct.getCode());
+			sb.append("\n");
+			sb.append("\t starting at position:" + ct.getStartPosition());
+			sb.append("\n");
+			sb.append("\t end at position:" + ct.getEndPosition());
+			sb.append("\n");
+			sb.append("\t first token number " + ct.getTokenStartPosition() + " out of " + (this.getTNoutput().getTokens().length - 1));
+			sb.append("\n");
+			sb.append("\t last token number " + ct.getTokenEndPosition() + " out of " + (this.getTNoutput().getTokens().length - 1));
+			sb.append("\n");
+			count = count + 1;
+		}
+		return(sb.toString());
+	}
 }
