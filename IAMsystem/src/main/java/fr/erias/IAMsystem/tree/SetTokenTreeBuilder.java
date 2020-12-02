1b5761c80adc6c064c256ff4772825825ef16e5e
cossins <sebastien.cossin@chu-bordeaux.fr>
2020-12-02 22:19:02 +0100

    add method to add manually a term to an existing tree

--- IAMsystem/src/main/java/fr/erias/IAMsystem/tree/SetTokenTreeBuilder.java
+++ IAMsystem/src/main/java/fr/erias/IAMsystem/tree/SetTokenTreeBuilder.java
@@ -9,28 +9,40 @@ import fr.erias.IAMsystem.tokenizernormalizer.TokenizerNormalizer;
 public class SetTokenTreeBuilder {
 	
 	/**
-	 * Create a tree data structure from a terminology 
+	 * Add a terminology to a tree 
 	 * @param terminology a representation of a {@link Terminology}
 	 * @param tokenTreeSet0 a previous {@link SetTokenTree} or an empty one (new SetTokenTree())
 	 * @param tokenizerNormalizer an instance to normalize and tokenize {@link TokenizerNormalizer}
 	 * @return
 	 */
 	public static SetTokenTree loadTokenTree(Terminology terminology, SetTokenTree tokenTreeSet0, ITokenizerNormalizer tokenizerNormalizer) {
-		IStopwords stopwords = tokenizerNormalizer.getNormalizer().getStopwords();
 		for (Term term : terminology.getTerms()) {
-			String normalizedLabel = term.getNormalizedLabel();
-			if (stopwords.isStopWord(normalizedLabel)) {
-				continue;
-			}
-			String code = term.getCode();
-			String[] tokensArray = tokenizerNormalizer.getTokenizer().tokenize(normalizedLabel);
-			tokensArray = IStopwords.removeStopWords(stopwords, tokensArray);
-			if (tokensArray.length == 0) {
-				continue;
-			}
-			TokenTree tokenTree = new TokenTree(null,tokensArray, code);
-			tokenTreeSet0.addTokenTree(tokenTree);
+			addTerm(term, tokenTreeSet0, tokenizerNormalizer);
 		}
+		return(tokenTreeSet0);
+	}
+	
+	/**
+	 * Add a term to an existing tree
+	 * @param term a {@link Term} to add to the terminology
+	 * @param tokenTreeSet0 a previous {@link SetTokenTree} or an empty one (new SetTokenTree())
+	 * @param tokenizerNormalizer an instance to normalize and tokenize {@link TokenizerNormalizer}
+	 * @return
+	 */
+	public static SetTokenTree addTerm(Term term, SetTokenTree tokenTreeSet0, ITokenizerNormalizer tokenizerNormalizer) {
+		IStopwords stopwords = tokenizerNormalizer.getNormalizer().getStopwords();
+		String normalizedLabel = term.getNormalizedLabel();
+		if (stopwords.isStopWord(normalizedLabel)) {
+			return(tokenTreeSet0);
+		}
+		String code = term.getCode();
+		String[] tokensArray = tokenizerNormalizer.getTokenizer().tokenize(normalizedLabel);
+		tokensArray = IStopwords.removeStopWords(stopwords, tokensArray);
+		if (tokensArray.length == 0) {
+			return(tokenTreeSet0);
+		}
+		TokenTree tokenTree = new TokenTree(null,tokensArray, code);
+		tokenTreeSet0.addTokenTree(tokenTree);
 		return(tokenTreeSet0);
 	}
 	
