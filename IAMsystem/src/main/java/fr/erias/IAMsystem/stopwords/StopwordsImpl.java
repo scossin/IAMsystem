889712d331b1f84e4fb476eaa0c0a2a480991d6a
cossins <sebastien.cossin@chu-bordeaux.fr>
2020-12-02 19:34:32 +0100

    improve stopwords class

--- IAMsystem/src/main/java/fr/erias/IAMsystem/stopwords/StopwordsImpl.java
+++ IAMsystem/src/main/java/fr/erias/IAMsystem/stopwords/StopwordsImpl.java
@@ -1,13 +1,15 @@
 package fr.erias.IAMsystem.stopwords;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.HashSet;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import fr.erias.IAMsystem.normalizer.INormalizer;
 
 /**
  * Very basic implementation of stopwords
@@ -23,9 +25,14 @@ public class StopwordsImpl implements IStopwords {
 	 */
 	private HashSet<String> stopwordsSet = new HashSet<String>();
 	
-	public StopwordsImpl() {}
+	/**
+	 * Constructor
+	 */
+	public StopwordsImpl() {
+		
+	}
 	
-	public StopwordsImpl( HashSet<String> stopwordsSet) {
+	public StopwordsImpl(HashSet<String> stopwordsSet) {
 		this.stopwordsSet = stopwordsSet;
 	}
 	
@@ -41,24 +48,46 @@ public class StopwordsImpl implements IStopwords {
 	}
 	
 	/**
-	 * Change the set of stopwords
+	 * Add stopwords from a file
 	 * @param stopwordsFile A file containing a list of stopword ; one by line
 	 * @throws IOException if the file can't be found
 	 */
-	public void setStopWords (File stopwordsFile) throws IOException {
+	public void setStopWords (InputStream in, INormalizer normalizer) throws IOException {
 		//ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
 		//File file = new File(classLoader.getResource(stopwordsFile).getFile());
 		BufferedReader br = null;
-		br = new BufferedReader(new FileReader(stopwordsFile));
+		br = new BufferedReader(new InputStreamReader(in));
 		String line = null;
 		while ((line = br.readLine()) != null) {
-			//String normalizedLabel = normalizedSentence(line);
-			stopwordsSet.add(line);
+			String normalizedLabel = normalizer.getNormalizedSentence(line);
+			stopwordsSet.add(normalizedLabel);
 		}
 		logger.info("stopwords size : " + stopwordsSet.size());
 		br.close();
 	}
 	
+	/**
+	 * Add stopwords from a file without normalization
+	 * @param in stopwordsFile A file containing a list of stopword ; one by line
+	 * @throws IOException
+	 */
+	public void setStopWords (InputStream in) throws IOException {
+		setStopWords(in, new INormalizer() {
+			@Override
+			public IStopwords getStopwords() {
+				return null;
+			}
+			
+			@Override
+			public String getNormalizedSentence(String sentence) {
+				return sentence;
+			}
+		});
+	}
+	
+	/**
+	 * Add a stopword
+	 */
 	public void addStopwords(String stopword) {
 		stopwordsSet.add(stopword);
 	}
