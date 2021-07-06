f4e32a05b63ac584ed0ecc14b14c0a62ffcb7a49
Sebastien Cossin <sebastien.cossin@chu-bordeaux.fr>
2021-07-06 23:00:02 +0200

    add Prefix test

--- IAMsystem/src/test/java/fr/erias/IAMsystem/stemmer/PrefixStemmerTest.java
+++ IAMsystem/src/test/java/fr/erias/IAMsystem/stemmer/PrefixStemmerTest.java
@@ -4,6 +4,11 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
+import fr.erias.IAMsystem.detect.DetectOutput;
+import fr.erias.IAMsystem.detect.TermDetector;
+import fr.erias.IAMsystem.synonym.Stems;
+import fr.erias.IAMsystem.terminology.Terminology;
+
 public class PrefixStemmerTest {
 
 	@Test
@@ -11,5 +16,21 @@ public class PrefixStemmerTest {
 		IStemmer stemmer = new StemByPrefix(5);
 		assertEquals(stemmer.stem("diabétique"), "diabé");
 		assertEquals(stemmer.stem("scannographiques"), "scann");
+	}
+	
+	@Test
+	public void detectionWithStemsTest() {
+		TermDetector termDetector = new TermDetector();
+		
+		Terminology terminology = new Terminology();
+		terminology.addTerm("diabete", "E11", termDetector.getTokenizerNormalizer().getNormalizer());
+		termDetector.addTerminology(terminology);
+		
+		IStemmer stemmer = new StemByPrefix(5);
+		Stems stems = new Stems(stemmer, terminology, termDetector.getTokenizerNormalizer());
+		termDetector.addSynonym(stems);
+		
+		DetectOutput detectOutput = termDetector.detect("le patient est diabétique");
+		assertEquals(detectOutput.getCTcodes().size(), 1);
 	}
 }
