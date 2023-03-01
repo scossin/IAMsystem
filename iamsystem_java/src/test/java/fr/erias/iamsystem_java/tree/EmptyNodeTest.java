fdab8ad1f7aa992b849fee443f101a3cc599d597
sebastien cossin <cossin.sebastien@gmail.com>
2023-02-28 21:48:00 -0300

    update pom.xml

--- iamsystem_java/src/test/java/fr/erias/iamsystem_java/tree/EmptyNodeTest.java
+++ iamsystem_java/src/test/java/fr/erias/iamsystem_java/tree/EmptyNodeTest.java
@@ -1,8 +1,8 @@
 package fr.erias.iamsystem_java.tree;
 
-import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
